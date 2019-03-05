b32132ec0ca1df8bb253abcf8f8c26e77c561a47
navarro.fabio@gmail.com <navarro.fabio@gmail.com>
2019-03-05 20:06:43 -0300

    first commit

---
+++ DDDCommerce.Domain/Account/Entities/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDDCommerce.Domain.Account.ValueObjects;
+
+namespace DDDCommerce.Domain.Account.Entities
+{
+    public class Student
+    {
+
+        public Name Name { get; private set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Document { get; set; }
+
+
+    }
+}
